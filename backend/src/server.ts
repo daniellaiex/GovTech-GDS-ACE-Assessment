86a44f051de18b5c0d2350b251cab02e980bccce
Daniel Lai <danfm28@gmail.com>
2024-02-20 01:20:32 +0800

    Base set up
    Test Exporess server and other base configurations

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+import env from "./util/validateEnv";
+import mongoose from 'mongoose';
+
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING!)
+  .then(() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+      console.log("Server running on port: " + port);
+    });
+  })
+  .catch(console.error);
