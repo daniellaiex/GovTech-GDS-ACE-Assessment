a6087ee4f4ff00ce7ba180af58e48f743c453471
Daniel Lai <danfm28@gmail.com>
2024-02-23 18:22:22 +0800

    Redeem models, controllers and routes
    Redeem Collection setup

---
+++ backend/src/controllers/redeem.ts
@@ -0,0 +1,40 @@
+import { RequestHandler } from "express";
+import RedeemModel from "../models/Redeem";
+import createHttpError from "http-errors";
+
+export const getAllTeams: RequestHandler = async (req, res, next) => {
+  try {
+    const teams = await RedeemModel.find().exec();
+
+    if (!teams) {
+      throw createHttpError(404, "Teams not found");
+    }
+
+    res.status(200).json(teams);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateTeamRedeemed: RequestHandler = async (req, res, next) => {
+  // get the team name from the request body
+  const teamName = req.params.teamName;
+
+  try {
+    if (!teamName) {
+      throw createHttpError(400, "Invalid Team Name");
+    }
+
+    const team = await RedeemModel.findOneAndUpdate({team_name: teamName}, {redeemed: true, updated_at: Date.now()}, {new: true}).exec();
+
+    if (!team) {
+      throw createHttpError(404, "Team not found");
+    }
+
+    const updatedTeams = await RedeemModel.find().exec();
+
+    res.status(200).json(updatedTeams);
+  } catch (error) {
+    next(error);
+  }
+};
