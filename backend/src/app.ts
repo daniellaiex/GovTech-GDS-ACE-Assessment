2b4307893f6d2e7a1b3f36552b0a1a5bbd012530
Daniel Lai <danfm28@gmail.com>
2024-02-23 18:41:58 +0800

    Session creation and API routing availability
    Set login session timeouts and expose API calls to frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,13 @@
+import MongoStore from 'connect-mongo';
 import 'dotenv/config';
-import express, { Request, Response, NextFunction } from 'express';
-import notesRoutes from './routes/notes';
-import morgan from 'morgan';
+import express, { NextFunction, Request, Response } from 'express';
+import session from 'express-session';
 import createHttpError, { isHttpError } from 'http-errors';
+import morgan from 'morgan';
+import redeemRoutes from './routes/redeem';
+import staffRoutes from './routes/staff';
+import userRoutes from './routes/users';
+import env from './util/validateEnv';
 
 const app = express();
 
@@ -10,7 +15,22 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
-app.use("/api/notes", notesRoutes);
+app.use(session({
+  secret: env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+  },
+  rolling: true,
+  store: MongoStore.create({
+    mongoUrl: env.MONGO_CONNECTION_STRING,
+  }),
+}));
+
+app.use("/api/staff", staffRoutes);
+app.use("/api/redeem", redeemRoutes);
+app.use("/api/users", userRoutes);
 
 app.use((req, res, next) => {
   next(createHttpError(404, "Endpoint not found"));
